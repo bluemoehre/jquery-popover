ce8e584615fc670c008c609f193957ebb505b787
Moehre <blue_moehre@gmx.de>
2014-05-08 02:52:56 +0200

    initial commit

---
+++ jquery.popover.js
@@ -0,0 +1,281 @@
+/**
+ * @license GNU General Public License v2 http://www.gnu.org/licenses/gpl-2.0
+ * @author BlueMöhre <bluemoehre@gmx.de>
+ * @copyright 2014 BlueMöhre
+ * @link http://www.github.com/bluemoehre
+ */
+
+// use window and document as local variables due to performance improvement
+(function($, win, doc) {
+
+    'use strict';
+
+    /**
+     * Plugin name and data-attr name (change for solving conflicts)
+     * @type {string}
+     */
+    var PLUGIN_NAME = 'popover';
+
+    /**
+     * Default options for this plugin
+     * @type {{Object}}
+     */
+    var defOpts = {
+        tpl: '<div class="popover"><h6>__headline__</h6><p>__text__</p></div>', // selector, jQuery-Collection or HTML
+        data: {
+            // data to fill the template
+            headline: 'This is a popover',
+            text: 'This is the default text for a popover.'
+        },
+        showOnHover: true,
+        showOnHoverDelay: 1000,
+        animSpeed: 100
+    };
+
+    /**
+     * Extended document
+     * @type {jQuery}
+     */
+    var $doc = $(doc);
+
+
+    /**
+     * Plugin constructor
+     * @param {HTMLElement} el
+     * @constructor
+     */
+    function Plugin(el)
+    {
+        /**
+         * The element which was passed to the plugin
+         * @type {jQuery}
+         */
+        var $el = $(el);
+
+        /**
+         * The plugin settings for this instance
+         * @type {Object}
+         */
+        var opts = {};
+
+        /**
+         * Self-reference
+         * @type {Plugin}
+         */
+        var self = this;
+
+        /**
+         * @type {jQuery}
+         */
+        var $popover;
+
+        /**
+         * Was popover shown by a click?
+         * @type {boolean}
+         */
+        var wasClicked = false;
+
+        /**
+         * Mouse hover delay timer
+         * @type {number}
+         */
+        var showTimeout = 0;
+
+        /**
+         * Mouse left delay timer
+         * @type {number}
+         */
+        var hideTimeout = 0;
+
+
+        /**
+         * Returns an escaped string
+         * Fastest version!
+         * @see http://jsperf.com/htmlencoderegex/25
+         * @param {string} text
+         * @returns {string}
+         */
+        function htmlEncode(text){
+            return document.createElement('div').appendChild(document.createTextNode(text)).parentNode.innerHTML;
+        }
+
+        /**
+         * Returns the current string where all placeholders have been replaced with the given data
+         * @param {string} html
+         * @param {object} data
+         * @returns {string}
+         */
+        function replacePlaceholders(html, data){
+            $.each(data, function(placeholder, value){
+                html = html.replace('__'+ placeholder +'__', htmlEncode(value));
+            });
+            return html;
+        }
+
+
+        /**
+         * Init function for setting up this instance
+         * The settings are cascaded in the following order:
+         *  - the plugin defaults
+         *  - the given options via jQuery-call
+         *  - the element options via attribute
+         *  (latest takes precedence)
+         *
+         * @param {Object} initOpts
+         */
+        this.init = function(initOpts){
+            var attrOptStr = $el.attr('data-'+ PLUGIN_NAME);
+            var attrOpts = attrOptStr ? $.parseJSON(attrOptStr) : {};
+            opts = $.extend(opts, defOpts, initOpts, attrOpts);
+
+            // add event handlers
+            $el.on('click.'+ PLUGIN_NAME, function(evt){
+                    evt.preventDefault();
+                    wasClicked = true;
+                    self.show();
+                })
+                .on('mouseenter.'+ PLUGIN_NAME, function(){
+                    clearTimeout(hideTimeout);
+                    showTimeout = setTimeout(function(){
+                        self.show();
+                    }, opts.showOnHoverDelay);
+                })
+                .on('mouseleave.'+ PLUGIN_NAME, function(){
+                    clearTimeout(showTimeout);
+                    if (!wasClicked){
+                        hideTimeout = setTimeout(function(){
+                            self.hide();
+                        }, opts.showOnHoverDelay);
+                    }
+                })
+
+        };
+
+        /**
+         * Remove this plugin off the element
+         * This function should revert all changes which have been made by this plugin
+         */
+        this.destroy = function(){
+            $doc.off('.' + PLUGIN_NAME);
+            $el.find('*').addBack().off('.' + PLUGIN_NAME);
+            $el.removeData(PLUGIN_NAME);
+            $el = null;
+        };
+
+        /**
+         * Show flyout
+         * @param {Object} [callOpts] extend the current option-set for this instance
+         */
+        this.show = function(callOpts){
+            opts = $.extend(opts, callOpts);
+            clearTimeout(showTimeout);
+
+            // if we have new settings remove old popover
+            if ($popover && callOpts){
+                $popover.remove();
+                $doc.off('click.'+ PLUGIN_NAME);
+            }
+
+            // if we have new settings rebuild popover
+            else if (callOpts) {
+
+                // build new popover
+                var $tpl = $(opts.tpl);
+                if ($tpl.is('script[type="text/template"]')){
+                    $popover = $(replacePlaceholders($tpl[0].innerHTML, opts.data));
+                } else {
+                    $popover = $(replacePlaceholders($tpl[0].outerHTML, opts.data));
+                }
+
+                // bind events
+                $popover
+                    .on('click.'+ PLUGIN_NAME, function(evt){
+                        evt.stopPropagation();
+                    })
+                    .on('mouseenter.'+ PLUGIN_NAME, function(evt){
+                        clearTimeout(hideTimeout);
+                    })
+                    .on('mouseleave.'+ PLUGIN_NAME, function(evt){
+                        if (!wasClicked){
+                            clearTimeout(showTimeout);
+                            hideTimeout = setTimeout(function(){
+                                self.hide();
+                            }, opts.showOnHoverDelay);
+                        }
+                    })
+            }
+
+            // if flyout is available and is not attached to the dom
+            if (!$popover.parent().length){
+                $popover
+                    .css({
+                        opacity: 0,
+                        position: 'absolute',
+                        left: $el.position().left + $el.outerWidth()/2 +'px',
+                        top: $el.position().top + $el.outerHeight() + parseInt($el.css('margin-top')) +'px'
+                    })
+                    .insertAfter($el)
+                    .css('margin-left', $popover.outerWidth()/2*-1 +'px')
+                    .stop(true)
+                    .fadeTo(opts.animSpeed, 1);
+
+                // delay event binding, so the click event for showing does not trigger close immediately
+                setTimeout(function(){
+                    $doc.one('click.'+ PLUGIN_NAME, function(){
+                        self.hide();
+                    })
+                }, 0);
+            }
+        };
+
+        /**
+         * Hide flyout
+         */
+        this.hide = function(){
+            clearTimeout(hideTimeout);
+            wasClicked = false;
+            if ($popover){
+                $popover.stop(true).fadeTo(opts.animSpeed, 0, function(){
+                    $popover.detach();
+                });
+            }
+        }
+    }
+
+
+
+    // Register plugin on jQuery
+    $.fn[PLUGIN_NAME] = function(){
+        var args = arguments;
+
+        return this.each(function(){
+
+            // Prevent multiple instances for same element
+            var instance = $.data(this, PLUGIN_NAME);
+            if (!instance){
+                instance = new Plugin(this);
+                $.data(this, PLUGIN_NAME, instance);
+                // init with settings object - handle
+                instance.init(typeof args[0] == 'object' ? args[0] : typeof args[1] == 'object' ? args[1] : {});
+            }
+            // Call public function
+            if (instance[args[0]]){
+                instance[args[0]](args[1]);
+            } else if (typeof args[0] === 'object' || !args[0]){
+                instance.init(args[0]);
+            } else {
+                $.error("Method '" + args[0] + "' doesn't exist for " + PLUGIN_NAME + " plugin");
+            }
+
+        });
+
+    };
+
+
+    // Auto pilot
+    $(doc).on('ready ajaxStop DOMContentAdded', function(evt, nodes){
+        $(nodes || document).find('[data-' + PLUGIN_NAME + ']').addBack('[data-' + PLUGIN_NAME + ']')[PLUGIN_NAME]();
+    });
+
+
+})(jQuery, window, document);
